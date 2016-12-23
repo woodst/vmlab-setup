91f7c2b0a3df8f90131d4fedf5624175a702df8b
Thomas M Woods <woodsth@regonstate.edu>
2016-12-22 17:35:56 -0800

    new file:   setup.sh
    Initial commit of setup.sh

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ======================================================
+# setup.sh
+# version 0.1.0
+# ------------------------------------------------------
+#
+# vmlab setup script
+# featured in the text "Build Your Own Virtualization 
+# Lab"
+#
+# ------------------------------------------------------
+# Copyright 2016, Thomas M. Woods
+# 
+# This script is distributed under the terms of the 
+# GNU Affero General Public License v3.0
+#
+# ======================================================
+
+
